24a994dcb9b637f9524693b59b920a2da00a5e3c
Quentin Bontemps <q.bontemps@gmail.com>
2013-10-17 12:38:25 +0200

    add partner name

---
+++ test/test_app.sh
@@ -0,0 +1,5 @@
+################ test_app.sh - 2013 #######################
+################ Quentin BONTEMPS ############################
+################ Thomas MONGREVILLE ##########################
+echo "Copyright company - 2013"
+echo "Version 1.2"
