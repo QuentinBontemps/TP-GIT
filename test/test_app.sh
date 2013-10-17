9ad36ed5c6384eeb2d1ee47a3d8802c9bdbd63e1
Quentin Bontemps <q.bontemps@gmail.com>
2013-10-17 14:27:35 +0200

    update major version

---
+++ test/test_app.sh
@@ -0,0 +1,8 @@
+################ application.sh - 2013 #######################
+################ Quentin BONTEMPS ############################
+################ Thomas MONGREVILLE ##########################
+version = "2.0.0"
+build_hour = "11H00"
+
+echo "Build ${build_hour}"
+echo "Version ${version}"
