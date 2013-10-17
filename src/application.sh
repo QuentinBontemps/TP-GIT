ae729d3b34f4d5ce569c63c7c45b2249313576bf
Quentin Bontemps <q.bontemps@gmail.com>
2013-10-17 12:03:29 +0200

    display copyright

--- src/application.sh
+++ src/application.sh
@@ -1,5 +1,6 @@
 ################ application.sh - 2013 #######################
 ################ Quentin BONTEMPS ############################
 ################ Thomas MONGREVILLE ##########################
+echo "Copyright company - 2013"
 echo "Build : 10:00"
-echo "Version 1"
+echo "Version 1.1"
